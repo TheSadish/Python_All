996334f9b1acc9b387a84a8bd2e23c43fd9bd84f
Sadish Kumar Dash <sadish.kumar.dash@accenture.com>
2024-12-26 18:24:56 +0530

    Variables

--- basics/variables.py
+++ basics/variables.py
@@ -1,6 +1,6 @@
 # Variable assignments
 
-var1 = "Sadish"
+var1 = "Sadish" # Other ways of string - var1 = 'Sadish', var1 = '''Sadish'''
 var2 = 25
 var3 = 15.5
 
@@ -23,5 +23,37 @@ print (type(w))
 print (type(x))
 print (type(y))
 
+str1 = "Dash"
 
+# List / Collection of variables - Enclosed in square brackets  - Mutable / Editable
 
+first_list = [str1, "Python", 2.45, 3, "Mama"]
+print (first_list)
+
+first_list[1] = "Not"  # List editing is possible
+print (first_list)
+
+# Tupple / Collection of variables - Enclosed in round brackets - Immutable / Non -Editable
+
+first_tuple = (str1, "Python", 2.45, 3, "Mama")
+print (first_tuple)
+
+# first_tuple[1] = "Not" # Tuple editing is not possible
+
+# Dictionary - Key Value Pairs - Curly brace
+
+first_dict = {
+    "Name":"Sadish",
+    "Age":25,
+    "Skills":["Python", "Azure", "Linux"]
+}
+
+print (first_dict)
+
+# Boolean
+
+x = True
+y = False
+
+if x == True:
+    print ("Yes")
