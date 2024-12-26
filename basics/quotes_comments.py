341e1e7ee18000387bea271ae0fd3eb5135e4d6e
Sadish Kumar Dash <sadish.kumar.dash@accenture.com>
2024-12-26 17:33:36 +0530

    Basic commits

---
+++ basics/quotes_comments.py
@@ -0,0 +1,16 @@
+#Types Of Comments ---->
+
+#Single Line Comment
+
+'''
+Multi Line Comment
+'''
+
+name = "Sadish"
+print (name)   # Print Variable
+print ("name") # Print speicific thing you want to print
+
+print ("""
+This is the first line,
+This is the second line of the paragraph    
+""")            # Print paragraphs 
---
+++ basics/variables.py
@@ -0,0 +1,27 @@
+# Variable assignments
+
+var1 = "Sadish"
+var2 = 25
+var3 = 15.5
+
+print (var1)
+print (var2)
+print (var3)
+print ()
+a = b = c = 65
+
+print (b)
+
+x, y, z, w = "alpha", "beta", 12, 5.4
+
+print ("Variable x value is", x, "of type", type(x))
+print ("Variable w value is", w)
+print ()
+
+# Get the data type
+print (type(w))
+print (type(x))
+print (type(y))
+
+
+
