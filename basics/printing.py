fdf640cb7e31a85e8f15dc2a8967c6379437c4a2
Sadish Kumar Dash <sadish.kumar.dash@accenture.com>
2024-12-26 18:36:20 +0530

    Printing Ways

---
+++ basics/printing.py
@@ -0,0 +1,13 @@
+name = "Sadish"
+age = 25
+
+print ("My name is", name, "any my age is", age)                # The old way 
+
+print ("My name is {} and my age is {}".format(name,age))       #  {} is replaced by format.(variable)
+
+print (f"My name is {name} and my age is {age}")                # New way + Simple (More than python 3.6)
+
+
+# Concatination (Java way)
+
+print ("My name is " + name + " and my age is " + str(age) )    # Java way. Need to convert to string for concatination
