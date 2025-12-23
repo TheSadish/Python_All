d251d521d5fc5718033203a4240b695afddf7cd8
Sadish Kumar Dash <sadishkumardash@gmail.com>
2025-12-23 22:34:52 +0530

    TWS Class 1

---
+++ The Basics/TWS.py
@@ -0,0 +1,18 @@
+print("Hello World") 
+
+num = {int(input("Write a number: "))}
+print(f"The input number was {num} \n") 
+
+# Type casting - Conversion of one data type to another data type.
+
+env = input("Enter the environment: ")
+print(f"The environment is {env}")  
+
+if (env == "Prod"):
+    print("You are in Production Environment")
+elif (env == "QA"):
+    print("You are in QA Environment")
+else:
+    print("You are in Dev Environment") 
+
+# Identation is very important in Python.
