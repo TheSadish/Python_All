0422aea55406310ad326f498e530ee135c14a0a5
Sadish Kumar Dash <sadishkumardash@gmail.com>
2025-12-31 12:21:24 +0530

    Action on S3 buckets

--- TWS/Cloud_Related/s3_utilities.py
+++ TWS/Cloud_Related/s3_utilities.py
@@ -10,6 +10,7 @@ def get_connection(service):
 
 def list_buckets(s3_client):
     my_all_buck = []
+    print("These are your current buckets - ")
     for bucket in s3_client.list_buckets()["Buckets"]:
         my_all_buck.append(bucket["Name"])
     return my_all_buck
@@ -29,6 +30,21 @@ def delete_buckets(s3_client, buckets):
             )
 
 s3 = get_connection("s3")
-buckets = list_buckets(s3)
 
-delete_buckets(s3,buckets)
+task = input("What do you want to do with bucket? Create, Delete or List ").lower()
+if task == "create":
+    bucket_name = input("Enter name of your bucket: ")
+    create_bucket(s3,bucket_name)
+    print("Bucket create successfully")
+elif task == "list":
+    list_buckets(s3)
+elif task == "delete":
+    list_buckets(s3)
+    bucket_name = input("Enter the bucket you want to delete among these: ")
+    delete_buckets(s3, bucket_name)
+else:
+    pass
+
+# buckets = list_buckets(s3)
+
+# delete_buckets(s3,buckets) - Deleting all buckets for now
